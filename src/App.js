78f8fdfa9c40f9ed879223ef0333ceb6115a1902
“haneul-png”  <kingdom616@naver.com>
2020-08-16 00:49:32 +0900

    동그라미

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button, Progress} from 'semantic-ui-react'
+import {Button, Progress, Image} from 'semantic-ui-react'
+
+import 구름 from "./구름.jpeg"
+
 
 function App() {
   return (
     <div>
   <p> 하늘 </p>
-  <Button Primary> hi </Button>
+  <Button > hi </Button>
   <Progress percent = {33} indicating/>
+  <Image src={구름} size='medium' centered circular />
+
+
   </div>
   );
 }
