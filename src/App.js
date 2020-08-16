e657a15de6666db9a307f990c102a8369d09039d
“haneul-png”  <kingdom616@naver.com>
2020-08-16 22:42:48 +0900

    그리드생성

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button, Progress, Image} from 'semantic-ui-react'
+import {Button, Progress, Image, Header} from 'semantic-ui-react'
+
+import GridLayout from './GridLayout.js'
 
 import 구름 from "./구름.jpeg"
+
 
 
 function App() {
   return (
     <div>
+    <Header as='h1'>First Header</Header>
+
   <p> 하늘 </p>
   <Button > hi </Button>
   <Progress percent = {33} indicating/>
+  <Image.Group size='medium'>
   <Image src={구름} size='medium' centered circular />
+  <Image src={구름} size='medium' centered circular />
+  <Image src={구름} size='medium' centered circular />
+  </Image.Group>
+
+  <GridLayout/>
+
+
 
 
   </div>
